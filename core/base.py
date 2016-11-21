6123e7bc66420102244f582e1d8fb3287efe2008
n3011 <mrinal.haloi11@gmail.com>
2016-11-21 23:34:01 +0530

    adding optimizer stuffs

--- core/base.py
+++ core/base.py
@@ -32,3 +32,27 @@ class Base(object):
             print "Target Set Success"
         else:
             print "Target Set Failed"
+
+    def optimizer(self, lr, optname='momentum', decay=0.9, momentum=0.9, epsilon=0.000000008, beta1=0.9, beta2=0.999):
+        """ definew the optimizer to use.
+
+        Args:
+            lr: learning rate, a scalar or a policy
+            optname: optimizer name
+            decay: variable decay value, scalar
+            momentum: momentum value, scalar
+
+        Returns:
+            optimizer to use
+         """
+        if optname == 'adadelta':
+            opt = tf.train.AdadeltaOptimizer(learning_rate=lr, rho=0.95, epsilon=1e-08, use_locking=False, name='Adadelta')
+        if optname == 'adagrad':
+            opt = tf.train.AdagradOptimizer(lr, initial_accumulator_value=0.1, use_locking=False, name='Adadelta')
+        if optname == 'rmsprop':
+            opt = tf.train.RMSPropOptimizer(lr, decay=0.9, momentum=0.0, epsilon=epsilon)
+        if optname == 'momentum':
+            opt = tf.train.MomentumOptimizer(lr, momentum, use_locking=False, name='momentum', use_nesterov=True)
+        if optname == 'adam':
+            opt = tf.train.AdamOptimizer(learning_rate=lr, beta1=beta1, beta2=beta2, epsilon=epsilon, use_locking=False, name='Adam')
+        return opt
