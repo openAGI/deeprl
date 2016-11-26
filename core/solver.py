153c2d53f70edba53b0f50e4dc8ac5561afed4ba
n3011 <mrinal.haloi11@gmail.com>
2016-11-27 01:14:44 +0530

    minor

--- core/solver.py
+++ core/solver.py
@@ -103,8 +103,7 @@ class Solver(Base):
 
                     print '\navg_r: %.4f, avg_l: %.6f, avg_q: %3.6f, avg_ep_r: %.4f, max_ep_r: %.4f, min_ep_r: %.4f, # game: %d' \
                         % (avg_reward, avg_loss, avg_q, avg_ep_reward, max_ep_reward, min_ep_reward, num_game)
-                    logger.debug('Avg_R: %.4f, Avg_L: %.6f, Avg_Q: %3.6f, Avg_EP_R: %.4f, Max_EP_R: %.4f, Min_EP_R: %.4f, # Game: %d' %
-                        (avg_reward, avg_loss, avg_q, avg_ep_reward, max_ep_reward, min_ep_reward, num_game))
+                    logger.debug('Step: %d, Avg_R: %.4f, Avg_L: %.6f, Avg_Q: %3.6f, Avg_EP_R: %.4f, Max_EP_R: %.4f, Min_EP_R: %.4f, # Game: %d' % (self.step, avg_reward, avg_loss, avg_q, avg_ep_reward, max_ep_reward, min_ep_reward, num_game))
 
                     if max_avg_ep_reward * 0.9 <= avg_ep_reward:
                         self.step_assign_op.eval({self.step_input: self.step + 1})
