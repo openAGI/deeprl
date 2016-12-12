45b6db393bc351bdb165d3fa4f3a37741e3d36d1
n3011 <mrinal.haloi11@gmail.com>
2016-12-12 11:05:04 +0530

    minor changes

--- models/ddpg_model.py
+++ models/ddpg_model.py
@@ -14,12 +14,12 @@ class ActorModel(object):
 
     def model_def(self, inputs, env, is_training=True, reuse=None, activation=tf.nn.relu, batchnorm=True, minval=-0.003, maxval=0.003, name=None):
         with tf.variable_scope('actor'):
-            fc1 = fc(inputs, 400, is_training, reuse, activation=activation)
+            fc1 = fc(inputs, 400, is_training, reuse, activation=activation, name=name + '/fc1')
             self.end_points['fc1'] = fc1
-            fc2 = fc(fc1, 300, is_training, reuse, activation=activation)
+            fc2 = fc(fc1, 300, is_training, reuse, activation=activation, name=name + '/fc2')
             self.end_points['fc2'] = fc2
             w_fc3_init = tf.random_uniform_initializer(minval=minval, maxval=maxval)
-            out = fc(fc2, env.action_dim, is_training, reuse, activation=activation, w_init=w_fc3_init)
+            out = fc(fc2, env.action_dim, is_training, reuse, activation=activation, w_init=w_fc3_init, name=name + '/out')
             self.end_points['out'] = out
             scaled_out = tf.mul(out, env.action_bound)
             self.end_points['scaled_out'] = scaled_out
@@ -32,13 +32,13 @@ class CriticModel(object):
 
     def model_def(self, inputs, action_inputs, is_training=True, reuse=None, activation=tf.nn.relu, batchnorm=True, minval=-0.003, maxval=0.003, name=None):
         with tf.variable_scope('critic'):
-            fc1 = fc(inputs, 400, is_training, reuse, activation=activation)
+            fc1 = fc(inputs, 400, is_training, reuse, activation=activation, name=name + '/fc1')
             self.end_points['fc1'] = fc1
-            fc2_temp = fc(fc1, 300, is_training, reuse, activation=None)
-            fc2_action = fc(action_inputs, 300, is_training, reuse, activation=None)
-            fc2 = activation(tf.matmul(fc1, fc2_temp.W) + tf.matmul(action_inputs, fc2_action.W) + fc2_action.b, name='relu')
+            fc2_temp = fc(fc1, 300, is_training, reuse, activation=None, name=name + '/fc2temp')
+            fc2_action = fc(action_inputs, 300, is_training, reuse, activation=None, name=name + '/fc2action')
+            fc2 = activation(tf.matmul(fc1, fc2_temp.W) + tf.matmul(action_inputs, fc2_action.W) + fc2_action.b, name=name + 'relu')
             self.end_points['fc2'] = fc2
             w_fc3_init = tf.random_uniform_initializer(minval=minval, maxval=maxval)
-            out = fc(fc2, 1, is_training, reuse, activation=activation, w_init=w_fc3_init)
+            out = fc(fc2, 1, is_training, reuse, activation=activation, w_init=w_fc3_init, name=name + '/out')
             self.end_points['out'] = out
             return self.end_points
